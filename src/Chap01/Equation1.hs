8802e6888e2ae45ab90725ce48c4a8842e414fb7
cutsea110 <cutsea110@gmail.com>
2021-08-18 07:18:00 +0900

    retrying

--- src/Chap01/Equation1.hs
+++ src/Chap01/Equation1.hs
@@ -28,27 +28,8 @@ script = do
   fmap snd $ withModel $ \m ->
     catMaybes <$> mapM (evalInt m) [x, y, z]
 
-
-
 data Expr = EVar String
           | EInt Integer
+          | EReal Double
           | EPlus Expr Expr
           deriving (Eq, Ord, Show)
-
-intVar :: MonadZ3 z3 => String -> State (Map.Map Expr (z3 AST)) (z3 AST)
-intVar name = do
-  m <- get
-  let (mayVal, m') = Map.insertLookupWithKey (\ _ n _ -> n) (EVar name) (mkFreshIntVar name) m
-  put m'
-  return $ m' Map.! EVar name
-
-plus :: MonadZ3 z3 => z3 AST -> z3 AST -> State (Map.Map AST (z3 AST))  (z3 AST)
-x `plus` y = return $ mkAdd =<< T.sequence [x,y]
-  
-e1 :: MonadZ3 z3 => AST -> AST -> AST -> AST -> AST -> AST -> z3 AST
-e1 x y z _1 _2 _3 = do
-  t1 <- mkMul [_3, x]
-  t2 <- mkMul [_2, y]
-  t3 <- mkUnaryMinus z
-  lhs <- mkAdd [t1,t2,t3]
-  mkEq _1 lhs
