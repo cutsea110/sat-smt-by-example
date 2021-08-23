cf9ca2b463fe518ebb5652ee30fce7449f93a781
cutsea110 <cutsea110@gmail.com>
2021-08-24 08:32:53 +0900

    define evalRel

--- src/Chap01/Equation1.hs
+++ src/Chap01/Equation1.hs
@@ -47,12 +47,32 @@ rel1 = EInt 1 `Eq` ((EInt 3 `ETimes` EVar "x") `EPlus` (EInt 2 `ETimes` EVar "y"
 rel2 = EInt (-2) `Eq` ((EInt 2 `ETimes` EVar "x") `EMinus` (EInt 2 `EPlus` EVar "y") `EPlus` (EInt 4 `ETimes` EVar "z"))
 rel3 = EInt 0 `Eq` (EVar "x" `EPlus` (EReal 0.5 `ETimes` EVar "y") `EMinus` EVar "z")
 
-evalRel :: MonadZ3 z3 => Map.Map Expr (z3 AST) -> Rel -> z3 (Map.Map Expr (z3 AST), AST)
-evalRel m (Eq lhs rhs) = undefined
-evalRel m (Lt lhs rhs) = undefined
-evalRel m (Gt lhs rhs) = undefined
-evalRel m (Le lhs rhs) = undefined
-evalRel m (Ge lhs rhs) = undefined
+evalRel :: MonadZ3 z3 => Map.Map Expr AST -> Rel -> z3 (Map.Map Expr AST, AST)
+evalRel m (Eq lhs rhs) = do
+  (m1, lhs') <- evalExpr m  lhs
+  (m2, rhs') <- evalExpr m1 rhs
+  rel <- mkEq lhs' rhs'
+  return (m2, rel)
+evalRel m (Lt lhs rhs) = do
+  (m1, lhs') <- evalExpr m  lhs
+  (m2, rhs') <- evalExpr m1 rhs
+  rel <- mkLt lhs' rhs'
+  return (m2, rel)
+evalRel m (Gt lhs rhs) = do
+  (m1, lhs') <- evalExpr m  lhs
+  (m2, rhs') <- evalExpr m1 rhs
+  rel <- mkGt lhs' rhs'
+  return (m2, rel)
+evalRel m (Le lhs rhs) = do
+  (m1, lhs') <- evalExpr m  lhs
+  (m2, rhs') <- evalExpr m1 rhs
+  rel <- mkLe lhs' rhs'
+  return (m2, rel)
+evalRel m (Ge lhs rhs) = do
+  (m1, lhs') <- evalExpr m  lhs
+  (m2, rhs') <- evalExpr m1 rhs
+  rel <- mkGe lhs' rhs'
+  return (m2, rel)
 
 evalExpr :: MonadZ3 z3 => Map.Map Expr AST -> Expr -> z3 (Map.Map Expr AST, AST)
 evalExpr m key@(EVar x)
