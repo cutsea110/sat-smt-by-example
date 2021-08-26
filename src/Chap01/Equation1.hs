bf84eb6ee6bf2cfc0139714ed40b5625191c622b
cutsea110 <cutsea110@gmail.com>
2021-08-26 10:24:55 +0900

    use infix operator instead of prefix

--- src/Chap01/Equation1.hs
+++ src/Chap01/Equation1.hs
@@ -37,9 +37,9 @@ simple :: Z3 (Maybe [Integer])
 simple = do
   let m0 = Map.empty
   m1 <- constraint m0
-        [ EInt 1 `Eq` ((EInt 3 `ETimes` EVar "x") `EPlus` (EInt 2 `ETimes` EVar "y") `EPlus` (EInt (-1) `ETimes` EVar "z"))
-        , EInt (-2) `Eq` ((EInt 2 `ETimes` EVar "x") `EPlus` (EInt (-2) `ETimes` EVar "y") `EPlus` (EInt 4 `ETimes` EVar "z"))
-        , EInt 0 `Eq` ((EInt (-1) `ETimes` EVar "x") `EPlus` (EReal 0.5 `ETimes` EVar "y") `EPlus` (EInt (-1) `ETimes` EVar "z"))
+        [ EInt 1 :==: ((EInt 3 `ETimes` EVar "x") `EPlus` (EInt 2 `ETimes` EVar "y") `EPlus` (EInt (-1) `ETimes` EVar "z"))
+        , EInt (-2) :==: ((EInt 2 `ETimes` EVar "x") `EPlus` (EInt (-2) `ETimes` EVar "y") `EPlus` (EInt 4 `ETimes` EVar "z"))
+        , EInt 0 :==: ((EInt (-1) `ETimes` EVar "x") `EPlus` (EReal 0.5 `ETimes` EVar "y") `EPlus` (EInt (-1) `ETimes` EVar "z"))
         ]
   fmap snd $ withModel $ \m ->
     catMaybes <$> mapM (evalInt m) (query' m1 [EVar "x", EVar "y", EVar "z"])
@@ -63,13 +63,6 @@ evalRels m0 rels = second reverse <$> foldM f (m0, []) rels
   where f (m,  rs) rel = do
           (m', r) <- evalRel m rel
           return (m', r:rs)
-{-
-evalRels m [] = return (m, [])
-evalRels m (rel:rels) = do
-  (m1, r)  <- evalRel m rel
-  (m2, rs) <- evalRels m1 rels
-  return (m2, r:rs)
--}
 
 {- |
 >>> evalZ3 test
@@ -78,10 +71,10 @@ Just [1,2]
 test :: Z3 (Maybe [Integer])
 test = do
   let m0 = Map.empty
-  (m1, r1) <- evalRel m0 (EVar "x" `Gt` EInt 0)
-  (m2, r2) <- evalRel m1 (EVar "y" `Gt` EInt 0)
-  (m3, r3) <- evalRel m2 (EVar "y" `Ge` EVar "x")
-  (m4, r4) <- evalRel m3 (EInt 3 `Eq` (EVar "x" `EPlus` EVar "y"))
+  (m1, r1) <- evalRel m0 (EVar "x" :>: EInt 0)
+  (m2, r2) <- evalRel m1 (EVar "y" :>: EInt 0)
+  (m3, r3) <- evalRel m2 (EVar "y" :>=: EVar "x")
+  (m4, r4) <- evalRel m3 (EInt 3 :==: (EVar "x" `EPlus` EVar "y"))
   assert =<< mkAnd [r1, r2, r3, r4]
   let (Just x) = Map.lookup (EVar "x") m4
   let (Just y) = Map.lookup (EVar "y") m4
@@ -96,40 +89,46 @@ data Expr = EVar String
           | ETimes Expr Expr
           deriving (Eq, Ord, Show)
 
-data Rel = Eq Expr Expr
-         | Lt Expr Expr
-         | Gt Expr Expr
-         | Le Expr Expr
-         | Ge Expr Expr
+data Rel = Expr :==: Expr
+         | Expr :/=: Expr
+         | Expr :<: Expr
+         | Expr :>: Expr
+         | Expr :<=: Expr
+         | Expr :>=: Expr
          deriving (Eq, Show)
 
 rel1, rel2, rel3 :: Rel
-rel1 = EInt 1 `Eq` ((EInt 3 `ETimes` EVar "x") `EPlus` (EInt 2 `ETimes` EVar "y") `EMinus` EVar "z")
-rel2 = EInt (-2) `Eq` ((EInt 2 `ETimes` EVar "x") `EPlus` (EInt (-2) `EPlus` EVar "y") `EPlus` (EInt 4 `ETimes` EVar "z"))
-rel3 = EInt 0 `Eq` ((EInt (-1) `ETimes` EVar "x") `EPlus` (EReal 0.5 `ETimes` EVar "y") `EPlus` (EInt (-1) `ETimes` EVar "z"))
+rel1 = EInt 1 :==: ((EInt 3 `ETimes` EVar "x") `EPlus` (EInt 2 `ETimes` EVar "y") `EMinus` EVar "z")
+rel2 = EInt (-2) :==: ((EInt 2 `ETimes` EVar "x") `EPlus` (EInt (-2) `EPlus` EVar "y") `EPlus` (EInt 4 `ETimes` EVar "z"))
+rel3 = EInt 0 :==: ((EInt (-1) `ETimes` EVar "x") `EPlus` (EReal 0.5 `ETimes` EVar "y") `EPlus` (EInt (-1) `ETimes` EVar "z"))
 
 evalRel :: MonadZ3 z3 => Map.Map Expr AST -> Rel -> z3 (Map.Map Expr AST, AST)
-evalRel m (Eq lhs rhs) = do
+evalRel m (lhs :==: rhs) = do
   (m1, lhs') <- evalExpr m  lhs
   (m2, rhs') <- evalExpr m1 rhs
   rel <- mkEq lhs' rhs'
   return (m2, rel)
-evalRel m (Lt lhs rhs) = do
+evalRel m (lhs :/=: rhs) = do
+  (m1, lhs') <- evalExpr m  lhs
+  (m2, rhs') <- evalExpr m1 rhs
+  rel  <- mkNot =<< mkEq lhs' rhs'
+  return (m2, rel)
+evalRel m (lhs :<: rhs) = do
   (m1, lhs') <- evalExpr m  lhs
   (m2, rhs') <- evalExpr m1 rhs
   rel <- mkLt lhs' rhs'
   return (m2, rel)
-evalRel m (Gt lhs rhs) = do
+evalRel m (lhs :>: rhs) = do
   (m1, lhs') <- evalExpr m  lhs
   (m2, rhs') <- evalExpr m1 rhs
   rel <- mkGt lhs' rhs'
   return (m2, rel)
-evalRel m (Le lhs rhs) = do
+evalRel m (lhs :<=: rhs) = do
   (m1, lhs') <- evalExpr m  lhs
   (m2, rhs') <- evalExpr m1 rhs
   rel <- mkLe lhs' rhs'
   return (m2, rel)
-evalRel m (Ge lhs rhs) = do
+evalRel m (lhs :>=: rhs) = do
   (m1, lhs') <- evalExpr m  lhs
   (m2, rhs') <- evalExpr m1 rhs
   rel <- mkGe lhs' rhs'
